9499166122f719d7773c003d458fdfc73695dc9e
Roberto Martinez <hdkrus.work@gmail.com>
2018-11-20 22:02:26 -0500

    add mutable concurrent queue

---
+++ Runes.Core.Test/Collections/Mutable/QueueTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Runes.Collections.Mutable;
+
+namespace Runes.Core.Test.Collections.Mutable
+{
+    [TestClass]
+    public class QueueTest
+    {
+        [TestMethod]
+        public void TestEmptyInvariants()
+        {
+            var queue = new Queue<int>();
+            Assert.IsTrue(queue.IsEmpty);
+            Assert.IsFalse(queue.NonEmpty);
+            Assert.IsTrue(queue.Peek().IsEmpty);
+            Assert.IsTrue(queue.Dequeue().IsEmpty);
+        }
+
+        [TestMethod]
+        public void TestEnqueue()
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(10);
+            Assert.IsTrue(queue.NonEmpty);
+            Assert.IsFalse(queue.IsEmpty);
+            Assert.IsTrue(queue.Peek().NonEmpty);
+            Assert.AreEqual(10, queue.Peek().GetOrElse(0));
+        }
+
+        [TestMethod]
+        public void TestDequeue()
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(10);
+            var opt = queue.Dequeue();
+            Assert.IsTrue(queue.IsEmpty);
+            Assert.IsFalse(queue.NonEmpty);
+            Assert.IsTrue(queue.Peek().IsEmpty);
+            Assert.AreEqual(10, opt.GetOrElse(0));
+        }
+    }
+}
---
+++ Runes.Core/Collections/Mutable/Queue.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Runes.Collections.Immutable;
+
+namespace Runes.Collections.Mutable
+{
+    public class Queue<A> : Traversable<A>
+    {
+        public Queue()
+        {
+            Head = Rear = new QueueNode(default, null);
+        }
+
+        public bool IsEmpty => Head.Next == null;
+        public bool NonEmpty => !IsEmpty;
+
+        public void Enqueue(A info)
+        {
+            lock (Rear)
+            {
+                Rear.Next = new QueueNode(info, null);
+                Rear = Rear.Next;
+            }
+        }
+
+        public Option<A> Peek()
+        {
+            lock(Head)
+            {
+                return InnerPeek();
+            }
+        }
+
+        public Option<A> Dequeue()
+        {
+            lock (Head)
+            {
+                var res = InnerPeek();
+                if (Head.Next != null)
+                {
+                    if (Head.Next == Rear)
+                    {
+                        lock(Rear)
+                        {
+                            if (Head.Next == Rear)
+                            {
+                                Rear = Head;
+                            }
+                        }
+                    }
+                    Head.Next = Head.Next.Next;
+                }
+                return res;
+            }
+        }
+
+        public override Unit Foreach(Action<A> action) => Unit.Of(() =>
+        {
+            var curr = Head.Next;
+            while (curr != null)
+            {
+                action(curr.Info);
+                curr = curr.Next;
+            }
+        });
+
+        public override Unit ForeachWhile(Func<A, bool> p, Action<A> action) => Unit.Of(() =>
+        {
+            var curr = Head.Next;
+            while (curr != null && p(curr.Info))
+            {
+                action(curr.Info);
+                curr = curr.Next;
+            }
+        });
+
+        public override IEnumerator<A> GetEnumerator() => ToList().GetEnumerator();
+
+        public override Stream<A> ToStream() => ToList().ToStream();
+
+        private QueueNode Head { get; set; }
+        private QueueNode Rear { get; set; }
+
+        private Option<A> InnerPeek() => Option.From(Head.Next).Map(n => n.Info);
+
+        private class QueueNode
+        {
+            public A Info { get; }
+            public QueueNode Next { get; set; }
+
+            public QueueNode(A info, QueueNode next)
+            {
+                Info = info;
+                Next = next;
+            }
+        }
+    }
+}
