b833f38a328ad4707037f63b48b9c0556d22a23e
Roberto Martinez <hdkrus.work@gmail.com>
2019-05-20 20:56:52 -0400

    add `Is` casting method

---
+++ Runes.Core.Test/CoreExtensionsTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Runes
+{
+    [TestClass]
+    public class CoreExtensionsTest
+    {
+        [TestMethod]
+        public void TestIsExtension()
+        {
+            object obj = "hello";
+
+            if (obj.Is(out string str))
+            {
+                Assert.AreEqual("hello", str);
+            }
+            else
+            {
+                Assert.Fail($"var {nameof(obj)} should be casted as string");
+            }
+        }
+    }
+}
---
+++ Runes.Core/CoreExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Runes
+{
+    public static class CoreExtensions
+    {
+        public static bool Is<A>(this object obj, out A castedObj)
+        {
+            castedObj = default;
+            var res = obj is A;
+            if (res)
+            {
+                castedObj = (A)obj;
+            }
+
+            return res;
+        }
+    }
+}
