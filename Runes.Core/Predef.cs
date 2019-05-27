ebb4d164a4956b68bc3391437a8aac28ca15fc6d
Roberto Martinez <hdkrus.work@gmail.com>
2019-05-26 21:42:38 -0400

    Add predef core extensions

---
+++ Runes.Core/Predef.cs
@@ -0,0 +1,9 @@
+﻿using static Runes.Options;
+
+namespace Runes
+{
+    public static class Predef
+    {
+        public static Option<A> As<A>(this object obj) => obj is A casted ? Some(casted) : None<A>();
+     }
+}
